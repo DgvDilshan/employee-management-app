424b7f09e661163ae4b3f82bb218ba71b18822e0
dgvdilshan@students.nsbm.ac.lk <vimukthidilshan23933@gmail.com>
2025-04-21 13:10:57 +0530

    start the task management app

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <h1>{{ msg }}</h1>
+    <p>{{ news }}</p>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+       news: 'my first news'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
